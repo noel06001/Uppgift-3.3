e66f94bf1d839e81bb0cf716c3d966c586c85516
noell <noell@DESKTOP-NLQ10I7>
2023-10-26 17:22:10 +0200

    Add project files.

---
+++ Uppgift 3.3/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Uppgift_3._3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hur många timmar vill du hyra bilen?");
+            int timmar = int.Parse(Console.ReadLine());
+
+            int pris = timmar * 80;
+            
+            if (pris > 950)
+            {
+                Console.WriteLine("Tyvär så överskrider detta den maximala hyran per dag." + " (950kr)");
+
+            }
+            else
+            {
+                Console.WriteLine("Den totala hyran blir " +  pris + "kr");
+            }
+            Console.ReadKey();
+        }
+    }
+}
+
